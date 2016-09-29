4661e50b534560c1c99889c434b0d96b8c03d923
SudheshRao <sudheshraop@gmail.com>
2016-09-29 22:06:51 +0530

    Revert "web-app"
    This reverts commit b879377f807efbec91b85bf4b9cc577b8350a4a8.

--- src/main/webapp/WEB-INF/resources/theme/js/navbar.js
+++
@@ -1,9 +0,0 @@
-                      $(window).scroll(function() {
-			if ($(document).scrollTop() > 150) {
-                alert('hi');
-			$('.logo').height(200);
-
-			}
-			else {
-    		$('.logo').height(100);
-			}});
